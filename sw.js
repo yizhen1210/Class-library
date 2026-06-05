5eb57f450bc07f07e7650c3c3773b7439ef73026
yizhen1210 <133864637+yizhen1210@users.noreply.github.com>
2026-06-05 17:09:35 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-// 最小 Service Worker：讓本站被視為正規 PWA。
-// 不做離線快取，網路請求一律由瀏覽器照常處理。
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => { /* 直接交給瀏覽器處理 */ });
